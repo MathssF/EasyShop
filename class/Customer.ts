6945c630c404cb221fe5c7b9287b48118241b1e0
Matheus Flores <Maths.Aki@gmail.com>
2025-02-28 15:36:32 -0300

    project start

---
+++ class/Customer.ts
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from "bcrypt";
+
+
+export class Customer {
+  private id: string;
+  public name: string;
+  public email: string;
+  public user: string;
+  private password: string;
+  public credit: number;
+  constructor(name: string, email: string, user: string, pass: string) {
+    this.id = uuidv4();
+    this.name = name;
+    this.email = email;
+    this.user = user;
+    this.password = pass;
+    this.credit = 0;
+  }
+}
