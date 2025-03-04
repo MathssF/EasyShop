feadfb3a619a40711c21193376afcd354affea68
Matheus Flores <Maths.Aki@gmail.com>
2025-03-04 17:27:01 -0300

    try

--- Testes/Run.ts
+++ Testes/Run.ts
@@ -78,22 +78,26 @@ import { ShopProduct } from "../Class/ShopProduct";
 // console.log('Agora exemplo de lista de clientes: ',
 // customers[2].showOrders(customers, order));
 
-
+console.log('Parte dos Produto');
 const product1 = new Product("Skol Beats")
 const product2 = new Product("Coca Zero")
 
-const shopProduct1 = new ShopProduct(product1, 10, 5, 0)
-const shopProduct2 = new ShopProduct(product2, 6, 2, 0)
+product1.detailsLog();
+product2.detailsLog();
 
+console.log('Parte dos Shop Produtos');
+const shopProduct1 = new ShopProduct(product1, 10, 50, 0)
+const shopProduct2 = new ShopProduct(product2, 6, 20, 0)
+
+console.log('Parte do Customer');
 const customer = new Customer("Lucas", "lcsflrs@gmail.com", "lcsflrs", "123123123")
 
+console.log('Parte dos pedidos')
 const orderItem1 = new OrderItem(2, shopProduct1)
 const orderItem2 = new OrderItem(3, shopProduct2)
 
-orderItem1.detailsLog()
-orderItem2.detailsLog()
+// orderItem1.detailsLog()
+// orderItem2.detailsLog()
 
-// const order = new Order(customer.id, [
-//   orderItem1, 
-//   orderItem2,
-// ])
+console.log('Parte final');
+const order = new Order(customer)
